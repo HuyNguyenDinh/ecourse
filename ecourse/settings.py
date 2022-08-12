9f45304e160116a2a9cd2744e2f94dbad10c4665
Dinh Huy <huyn27316@gmail.com>
2022-08-12 13:13:52 +0700

    fix

--- ecourse/settings.py
+++ ecourse/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
